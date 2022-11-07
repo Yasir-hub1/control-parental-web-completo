632d823293d11165fc3364ac5f939bfa20e50804
Carlos06929 <juanpi06929@gmail.com>
2022-11-06 22:23:45 -0400

    create maps google 1.0

--- resources/views/pruebas/ubicacion.blade.php
+++ resources/views/pruebas/ubicacion.blade.php
@@ -2,20 +2,10 @@
 
 
 @section('content')
-<br>
-<div class="container-fluid d-flex justify-content-center aling-items-center" style="height: 750px;">
-    <div class="card w-100">
-        <div class="card-header">
-          <h4 class="d-flex justify-content-center text-uppercase"><u><strong>Ubicacion de {{$info->name}} </strong></u></h4>
-            
-        </div>
-        <div class="card-body">
-            <iframe class="w-100 h-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9035.758464450946!2d-63.16046720096668!3d-17.78847979944152!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x3998764f83dfd833!2sCENTRO%20COMERCIAL%20BARRIO%20LINDO!5e0!3m2!1ses!2sbo!4v1667176072596!5m2!1ses!2sbo" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>                    
-        </div>
-    </div>
 
-</div>
-
+                <div id="map"> </div> 
+    
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAl8DaopxOLYwyY0gJV2fUky4_X99ZFwJY&callback=initMap" async defer></script>
 
 
   
@@ -29,6 +19,21 @@
 @stop
 
 @section('js')
-<script src=""></script>
 
+
+<script>
+        var map;
+  	 function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+		  center: {lat: 43.5293, lng: -5.6773},
+          zoom: 13,
+        });
+        var marker = new google.maps.Marker({
+          position: {lat: 43.542194, lng: -5.676875},
+          map: map,
+	  title: 'Acuario de Gijón'
+        });
+      }
+
+</script>
 @stop
