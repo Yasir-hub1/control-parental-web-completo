61f5a4fca2fbf3af52c705a41b7e4ae71c676903
angelicamiranda <angelicamirandau@gmail.com>
2022-12-16 23:19:22 -0400

    frontend

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -24,36 +24,144 @@
     <!--end::Global Stylesheets Bundle-->
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 </head>
+<!--end::Head-->
+<!--begin::Body-->
 
-<body>
+<body id="kt_body"
+    class="header-fixed header-tablet-and-mobile-fixed toolbar-enabled toolbar-fixed aside-enabled aside-fixed"
+    style="--kt-toolbar-height:55px;--kt-toolbar-height-tablet-and-mobile:55px">
+    <!--begin::Main-->
+    <!--begin::Root-->
+    <div class="d-flex flex-column flex-root">
+        <!--begin::Page-->
+        <div class="page d-flex flex-row flex-column-fluid">
+            <!--begin::Aside-->
+            <div id="kt_aside" class="aside aside-dark aside-hoverable" data-kt-drawer="true" data-kt-drawer-name="aside"
+                data-kt-drawer-activate="{default: true, lg: false}" data-kt-drawer-overlay="true"
+                data-kt-drawer-width="{default:'200px', '300px': '250px'}" data-kt-drawer-direction="start"
+                data-kt-drawer-toggle="#kt_aside_mobile_toggle">
+                <!--begin::Brand-->
+                <div class="aside-logo flex-column-auto" id="kt_aside_logo">
+                    <!--begin::Logo-->
+                    <a href="#">
+                        <img alt="Logo" src="{{asset('img/logowhite.png')}}" class="h-70px logo" />
+                    </a>
+                    <!--end::Logo-->
+                    <!--begin::Aside toggler-->
+                    <div id="kt_aside_toggle" class="btn btn-icon w-auto px-0 btn-active-color-primary aside-toggle"
+                        data-kt-toggle="true" data-kt-toggle-state="active" data-kt-toggle-target="body"
+                        data-kt-toggle-name="aside-minimize">
+                        <!--begin::Svg Icon | path: icons/duotune/arrows/arr079.svg-->
+                        <span class="svg-icon svg-icon-1 rotate-180">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                fill="none">
+                                <path opacity="0.5"
+                                    d="M14.2657 11.4343L18.45 7.25C18.8642 6.83579 18.8642 6.16421 18.45 5.75C18.0358 5.33579 17.3642 5.33579 16.95 5.75L11.4071 11.2929C11.0166 11.6834 11.0166 12.3166 11.4071 12.7071L16.95 18.25C17.3642 18.6642 18.0358 18.6642 18.45 18.25C18.8642 17.8358 18.8642 17.1642 18.45 16.75L14.2657 12.5657C13.9533 12.2533 13.9533 11.7467 14.2657 11.4343Z"
+                                    fill="black" />
+                                <path
+                                    d="M8.2657 11.4343L12.45 7.25C12.8642 6.83579 12.8642 6.16421 12.45 5.75C12.0358 5.33579 11.3642 5.33579 10.95 5.75L5.40712 11.2929C5.01659 11.6834 5.01659 12.3166 5.40712 12.7071L10.95 18.25C11.3642 18.6642 12.0358 18.6642 12.45 18.25C12.8642 17.8358 12.8642 17.1642 12.45 16.75L8.2657 12.5657C7.95328 12.2533 7.95328 11.7467 8.2657 11.4343Z"
+                                    fill="black" />
+                            </svg>
+                        </span>
+                        <!--end::Svg Icon-->
+                    </div>
+                    <!--end::Aside toggler-->
+                </div>
+                <!--end::Brand-->
+                <!--begin::Aside menu-->
+                <div class="aside-menu flex-column-fluid">
+                    <!--begin::Aside Menu-->
+                    <div class="hover-scroll-overlay-y my-5 my-lg-5" id="kt_aside_menu_wrapper" data-kt-scroll="true"
+                        data-kt-scroll-activate="{default: false, lg: true}" data-kt-scroll-height="auto"
+                        data-kt-scroll-dependencies="#kt_aside_logo, #kt_aside_footer"
+                        data-kt-scroll-wrappers="#kt_aside_menu" data-kt-scroll-offset="0">
+                        <!--begin::Menu-->
+                        <div class="menu menu-column menu-title-gray-800 menu-state-title-primary menu-state-icon-primary menu-state-bullet-primary menu-arrow-gray-500"
+                            id="#kt_aside_menu" data-kt-menu="true" data-kt-menu-expand="false">
 
-
-    <nav class="navbar navbar-expand-lg navbar-light bg-light ">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#" style=""> <img src="img/controlparental2.png" width="150">
-            </a>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="{{ route('perfil') }}">Perfil</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="{{ route('tokens') }}">Tokens de
-                            Usuario</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page"
-                            href="{{ route('dispositivos') }}">Dispositivos</a>
-                    </li>
-                    {{--<li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="{{ route('plan') }}">Plan</a>
-                    </li>--}}
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page"
-                            href="{{ route('notification.index') }}">Notificaciones</a>
-                    </li>
-
-                </ul>
+                            <div class="menu-item">
+                                <div class="menu-content pt-8 pb-2">
+                                    <span class="menu-section text-muted text-uppercase fs-8 ls-1">Opciones</span>
+                                </div>
+                            </div>
+                            <div data-kt-menu-trigger="click" class="menu-item menu-accordion">
+                                
+                             
+                                   
+                                       
+                                <div class="menu-item">
+                                    <a class="menu-link" href="{{route('perfil')}}">
+                                        <span class="menu-bullet">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-square" viewBox="0 0 16 16">
+                                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                                                <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1v-1c0-1-1-4-6-4s-6 3-6 4v1a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12z"/>
+                                              </svg>
+                                        </span>
+                                        <span class="menu-title">Perfil</span>
+                                    </a>
+                                </div>
+                    
+                                            <div class="menu-item">
+                                                <a class="menu-link" href="{{route('menu')}}">
+                                                    <span class="menu-bullet">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-heart" viewBox="0 0 16 16">
+                                                            <path d="M8 6.982C9.664 5.309 13.825 8.236 8 12 2.175 8.236 6.336 5.309 8 6.982Z"/>
+                                                            <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.707L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.646a.5.5 0 0 0 .708-.707L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z"/>
+                                                          </svg>
+                                                    </span>
+                                                    <span class="menu-title">Mi Familia</span>
+                                                </a>
+                                            </div>
+                                            <div class="menu-item">
+                                                <a class="menu-link" href="{{route('notification.index')}}">
+                                                    <span class="menu-bullet">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell-fill" viewBox="0 0 16 16">
+                                                            <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z"/>
+                                                          </svg>
+                                                      </span>
+                                                    <span class="menu-title">Notificaciones</span>
+                                                </a>
+                                            </div>
+                                            <div class="menu-item">
+                                                <a class="menu-link" href="{{route('tokens')}}">
+                                                    <span class="menu-bullet">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-phone-fill" viewBox="0 0 16 16">
+                                                            <path d="M3 2a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V2zm6 11a1 1 0 1 0-2 0 1 1 0 0 0 2 0z"/>
+                                                          </svg>
+                                                      </span>
+                                                    <span class="menu-title">Tokens</span>
+                                                </a>
+                                            </div>
+                                           
+                                           
+                              
+                                
+                            </div>
+                            
+                            
+                           
+                        </div>
+                        <!--end::Menu-->
+                    </div>
+                    <!--end::Aside Menu-->
+                </div>
+                <!--end::Aside menu-->
+                <!--begin::Footer-->
+                <div class="aside-footer flex-column-auto pt-5 pb-7 px-5" id="kt_aside_footer">
+                    <a href="{{ route('logout') }}" class="btn btn-custom btn-primary w-100"
+                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        <span class="btn-label">Cerrar Sesión</span>
+                        <!--begin::Svg Icon | path: icons/duotune/general/gen005.svg-->
+                        <span class="svg-icon btn-icon svg-icon-2">
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                style="display: none;">
+                                {{ csrf_field() }}
+                            </form>
+                        </span>
+                        <!--end::Svg Icon-->
+                    </a>
+                </div>
+                <!--end::Footer-->
             </div>
             <!--end::Aside-->
             <!--begin::Wrapper-->
@@ -336,28 +444,28 @@
       integrity="sha512-Zq9o+E00xhhR/7vJ49mxFNJ0KQw1E1TMWkPTxrWcnpfEFDEXgUiwJHIKit93EW/XxE31HSI5GEOW06G6BF1AtA=="
       crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-    <script>
-        // Enable pusher logging - don't include this in production
-        //  Pusher.logToConsole = true;
+  <script>
+    // Enable pusher logging - don't include this in production
+    //  Pusher.logToConsole = true;
 
-        var pusher = new Pusher('37043fdf6f86d96c9095', {
-            cluster: 'us2'
-        });
+    var pusher = new Pusher('109c670c473fed2434ca', {
+        cluster: 'us2'
+    });
 
     var channel = pusher.subscribe('my-channel');
     channel.bind('my-event', function(data) {
         console.log('entra');
         console.log(data);
 
-            //cambiando html con notificación nueva
-            //cambiando html con unread notification sin la notification nueva
-            if (data['unread'].length == 2) {
-                $('.unread-notification').find('#1').html('<div class="row">' +
-                    '<div class="col-12"><i class="fas fa-envelope mr-2"></i>' + data[
-                        'contenido']['nombre'] + '</div>' +
-                    '<div class="col-12"><small class="ml-2 float-end text-muted text-sm"' +
-                    ' style="font-size: 0.6rem">' + data['time'] + '</small>' +
-                    '</div>' + '</div>');
+        //cambiando html con notificación nueva
+        //cambiando html con unread notification sin la notification nueva
+        if (data['unread'].length == 2) {
+            $('.unread-notification').find('#1').html('<div class="row">' +
+                '<div class="col-12"><i class="fas fa-envelope mr-2"></i>' + data[
+                    'contenido']['contenido'] + '</div>' +
+                '<div class="col-12"><small class="ml-2 float-end text-muted text-sm"' +
+                ' style="font-size: 0.6rem">' + data['time'] + '</small>' +
+                '</div>' + '</div>');
 
             for (let index = 0; index < 2; index++) {
                 console.log('notificacion sin leer: ' + data['unread'][index]['nombre']);
@@ -429,24 +537,21 @@
 
             $('.count-notification').text(c);
 
-                iziToast.show({
-                    title: '¡Nueva Notificación!',
-                    message: data['contenido']['nombre'],
-                    backgroundColor: 'red',
-                    theme: 'dark', // dark
-                    color: 'red', // blue, red, green, yellow
-                    timeout: 10000,
-                    overlayClose: false,
-                });
-            }
-        });
-    </script>
-
-    </head>
-
-
-
-
+            iziToast.show({
+                title: '¡Nueva Notificación!',
+                message: data['contenido']['contenido'],
+                backgroundColor: 'red',
+                theme: 'dark', // dark
+                color: 'red', // blue, red, green, yellow
+                timeout: 10000,
+                overlayClose: false,
+            });
+        }
+    });
+</script>
+  @yield('js')
+    <!--end::Page Custom Javascript-->
+    <!--end::Javascript-->
 </body>
 <!--end::Body-->
 
