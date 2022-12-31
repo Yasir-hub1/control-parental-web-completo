6db576ac41394429b5c1489e1628d1c0804af87e
Yasir <yasirarce70@gmail.com>
2022-12-31 03:07:54 -0400

    Cambio de idioma al enviar notificaciones

--- app/Listeners/NotificationContenidoListener.php
+++ app/Listeners/NotificationContenidoListener.php
@@ -32,6 +32,47 @@ class NotificationContenidoListener implements ShouldQueue
      */
     public function handle($event)
     {
+        /* array para contenidos */
+        $nameData=array(
+            'Nudity'=>'Desnudez',
+            'Graphic Male Nudity'=>'Desnudez Masculina Gráfica',
+            'Graphic Female Nudity'=>'Desnudez femenina gráfica',
+            'Sexual Activity'=>'Actividad Sexual',
+            'Illustrated Explicit Nudity'=>'Desnudez explícita ilustrada',
+            'Adult Toys'=>'Juguetes para adultos',
+            'Female Swimwear Or Underwear'=>'Ropa interior femenino',
+            'Male Swimwear Or Underwear'=>'Ropa interior masculino',
+            'Partial Nudity'=>'Desnudez parcial',
+            'Barechested Male'=>'Hombre con el torso desnudo',
+            'Revealing Clothes'=>'Atuendo revelador',
+            'Sexual Situations'=>'Situaciones Sexuales',
+            'Graphic Violence Or Gore'=>'Violencia Gráfica O Gore',
+            'Physical Violence'=>'Violencia física',
+            'Weapon Violence'=>'Violencia con Armas',
+            'Weapons'=>'Armas',
+            'Self Injury'=>'Auto lastimarse',
+            'Emaciated Bodies'=>'Cuerpos demacrados',
+            'Corpses'=>'Cuerpos',
+            'Hanging'=>'Colgante',
+            'Air Crash'=>'Accidente aéreo',
+            'Explosions And Blasts'=>'Explosiones y explosiones',
+            'Middle Finger'=>'Dedo del medio',
+            'Drug Products'=>'Productos farmacéuticos',
+            'Drug Use'=>'El consumo de drogas',
+            'Pills'=>'Pastillas',
+            'Drug Paraphernalia'=>'Parafernalia de drogas',
+            'Tobacco Products'=>'Productos de tabaco',
+            'Smoking'=>'Fumar',
+            'Drinking'=>'Bebiendo',
+            'Alcoholic Beverages'=>'Bebidas alcohólicas',
+            'Gambling'=>'Juego',
+            'Nazi Party'=>'Fiesta nazi',
+            'White Supremacy'=>'La supremacía blanca',
+            'Extremist'=>'Extremista',
+            );
+
+
+
         //para el tutor del hijo
         // $hijo = Hijo::find($event->contenido['hijo_id']);
         $user = User::find($event->user['id']);
@@ -45,7 +86,7 @@ class NotificationContenidoListener implements ShouldQueue
             // Subscribe the recipient to the server
             $expo->subscribe('canal', $recipient);
             // Build the notification data
-            $notification = ['body' => $event->contenido->contenido, 'title' => 'Protecting you', 'ttl' => 60, 'Sound' => 'Default','data'=>$data];
+            $notification = ['body' =>"Tu hij@ está mirando ".$nameData[$event->contenido->contenido], 'title' => 'Protecting You', 'ttl' => 60, 'Sound' => 'Default','priority'=>'high','data'=>$data];
             // Notify an interest with a notification
             $expo->notify(['canal'], $notification);
         }
